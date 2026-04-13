a91dfaec0b0fc18a126d7b6c36c3e006ad915fb3
Mathias Klippinge <mathias.klippinge@gmail.com>
2026-04-13 17:41:04 +0200

    Remove iterm2 stuff

--- setup-osx.sh
+++ setup-osx.sh
@@ -29,6 +29,3 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
 defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
 defaults write com.apple.commerce AutoUpdate -bool true
-
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/repos/dotfiles/osx/iterm2"
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
